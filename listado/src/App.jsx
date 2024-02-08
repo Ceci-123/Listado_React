4b2d3ba633c80c84b889fb2e844996ceb9e24092
Ceci <78119165+Ceci-123@users.noreply.github.com>
2024-02-07 21:20:29 -0300

    agrego json

--- listado/src/App.jsx
+++ listado/src/App.jsx
@@ -1,11 +1,25 @@
 //import React from 'react'
 import './App.css'
 import Cards from '../components/cards'
+import result from './assets/result.json'
 
 function App() {
+  const movies = result.Search
   return (
     <>
      <Cards/>
+     <main>
+      
+      <ul>
+        {movies.map(movie =>(
+          <li key={movie.Id}>
+            <h3>{movie.Title}</h3>
+            <h4>{movie.Type}</h4>
+            <h5>{movie.Cantidad}</h5>
+          </li>
+        ))}
+      </ul>
+     </main>
     </>
   )
 }
